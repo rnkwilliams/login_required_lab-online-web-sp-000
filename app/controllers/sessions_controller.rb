accf0a6072233b2c49aff8324e4df194ccb524ca
Ranika Williams <rnkprice07@gmail.com>
2019-12-16 17:49:16 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,12 @@ class SessionsController < ApplicationController
   end
   
   def create
-    
+    if params[:name].nil? || params[:name].empty?
+      redirect_to :login
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
   end
   
   def destroy
