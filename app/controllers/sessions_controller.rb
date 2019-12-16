e60e880672045fdb9c803825b93ece8bac58f34b
Ranika Williams <rnkprice07@gmail.com>
2019-12-16 20:49:30 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to :login
+    end
+  end
+  
+  def destroy
+    if session[:name].present?
+      session.delete :name
+    else
+      redirect_to :login
+    end
+  end
+end
