ae9168dfbb8221da2f8871327d3157dcb7278a28
Ranika Williams <rnkprice07@gmail.com>
2019-12-16 17:58:53 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
     if !session[:name].present?
       session.delete :name
     end
-      redirect_to 'login'
+      redirect_to :login
     end
   end
   
